2698e4e1a21845cf1eaeffbf0c4d5fbf8efee13b
Alberto Hernández Ángel <>
2024-11-08 12:09:19 -0600

    Cambios para revisión

--- index.php
+++ index.php
@@ -4,6 +4,8 @@
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 <title> anna terrazas </title>
 
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+
 <link href="css/ed-grid.css" rel="stylesheet" type="text/css" />
 <link rel="stylesheet" href="plugins/jquery.bxslider/jquery.bxslider.css" />
 <link href="style.css" rel="stylesheet" type="text/css" />
@@ -68,56 +70,67 @@
 </div>
 
 <div class="contenido_diferencia" id="diferencia">
-	<img src="img/preload-galeria.gif" alt="" class="preload-galeria">
+	<!-- <img src="img/preload-galeria.gif" alt="" class="preload-galeria"> -->
 	<div class="work__slider">
 
 	</div>
 
 	<div class="work__menu">
-		<div class="grupo total">
+		<div class="container-fluid">
+			<div class="row">
+				<div class="col-9 p-3">
+					<div class="row">
+						<div class="col titulo-galeria">
+							features
+						</div>
+					</div>
+					<div class="d-flex">
+						<div class="col flex-grow-1 enlaces">
+							<a href="#" alt="work-bayoneta.html">Bayoneta</a><br>
+							<a href="#" alt="work-hello-tomorrow.html">Hello Tomorrow</a><br>
+							<a href="#" alt="work-bardo.html">Bardo</a><br>
+							<a href="#" alt="work-spectre.html">Spectre</a><br>
+							<span href="#" alt="">Eddington</span><br>
+
+						</div>
+
+						<div class="col flex-grow-1 enlaces">
+							<a href="#" alt="work-life-of-crime.html">Life of Crime</a><br>
+							<a href="#" alt="work-days-and-nights.html">Days and Nights</a><br>
+							<a href="#" alt="work-europa-report.html">Europa Report</a><br>
+							<a href="#" alt="work-miss-bala.html">Miss Bala</a><br>
+							<a href="#" alt="work-get-the-gringo.html">Get The Gringo</a><br>
+						</div>
+
+						<div class="col flex-grow-1 enlaces">
+							<a href="#" alt="work-solo-quiero-caminar.html">Solo Quiero Caminar</a><br>
+							<a href="#" alt="work-abel.html">Abel</a><br>
+							<a href="#" alt="work-rudo-y-cursi.html">Rudo y Cursi</a><br>
+							<span href="#" alt="work-get-the-gringo.html">The Border</span><br>
+						</div>
+					</div>
+				</div>
+
+				<div class="col-3 p-3">
+					<div class="row">
+						<div class="col titulo-galeria">
+							television
+						</div>
+					</div>
+					<div class="d-flex">
+						<div class="col flex-grow-1 enlaces">
+							<a href="#" alt="work-barkskins.html">Barkskins</a><br>
+							<a href="#" alt="work-the-bridge.html">The Bridge</a><br>
+						</div>
+					</div>
+				</div>
+			</div>
+
 			
-			<div class="television caja tablet-75">
-				<div class="caja titulo-galeria">
-					features
-				</div>
-
-				<div class="caja tablet-1-3 enlaces">
-					<a href="#" alt="work-bayoneta.html">Bayoneta</a><br>
-					<a href="#" alt="work-hello-tomorrow.html">Hello Tomorrow</a><br>
-					<a href="#" alt="work-bardo.html">Bardo</a><br>
-					<a href="#" alt="work-spectre.html">Spectre</a><br>
-					<span href="#" alt="">Eddington</span><br>
-
-				</div>
-
-				<div class="caja tablet-1-3 enlaces">
-					<a href="#" alt="work-life-of-crime.html">Life of Crime</a><br>
-					<a href="#" alt="work-days-and-nights.html">Days and Nights</a>
-					<a href="#" alt="work-europa-report.html">Europa Report</a><br>
-					<a href="#" alt="work-miss-bala.html">Miss Bala</a><br>
-					<a href="#" alt="work-get-the-gringo.html">Get The Gringo</a><br>
-					<span href="#" alt="work-get-the-gringo.html">The Border</span>
-				</div>
-
-				<div class="caja tablet-1-3 enlaces">
-					<a href="#" alt="work-solo-quiero-caminar.html">Solo Quiero Caminar</a><br>
-					<a href="#" alt="work-abel.html">Abel</a><br>
-					<a href="#" alt="work-rudo-y-cursi.html">Rudo y Cursi</a>
-				</div>
-			</div>
-
-
-			<div class="television caja tablet-25">
-				<div class="caja titulo-galeria">
-					television
-				</div>
-				<div class="caja enlaces">
-					<a href="#" alt="work-barkskins.html">Barkskins</a><br>
-					<a href="#" alt="work-the-bridge.html">The Bridge</a>
-				</div>
-			</div>
-
 		</div>
+	
+
+		
 	</div>
 
 </div>
@@ -129,8 +142,10 @@
 
 <div class="contenido_ideologia grupo" id="ideologia">
 	
-	<div class="caja tablet-100">
-		<img src="img/resume-content.jpg" alt="" />
+	<div class="row">
+		<div class="col p-5">
+			<img src="img/resume-content.jpg" alt="" />
+		</div>
 	</div>
 
 	<!--
@@ -181,22 +196,10 @@
 
 
 
-		<img src="img/logo-artistry.png" class="logo-contacto">
+		<img src="img/murthaskouras_logo.png" class="logo-contacto">
 		
 
-		AGENT<br>
-		Robin Sheldon<br>
-    	Jane Prosnit<br>
-    	<!-- Sheldon Prosnit Agency<br> -->
-    	<br>
-		
-
-		310 652 8778<br><br>
-
-		robin@artistry.net<br><br>
-
-		800 S. Robertson Blvd.  Ste #6<br>
-		Los Angeles, CA 90035<br><br>
+		<br><br>
 	</div>
 </div>
 
